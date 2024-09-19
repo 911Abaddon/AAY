8693573fefcfbc879d0b56de68a77ffee64fe721
Stelios12345 <steliosman7@gmail.com>
2024-09-20 00:13:43 +0300

    update Exhibition

--- AAY/ExhibitionSpaceManagement.cs
+++ AAY/ExhibitionSpaceManagement.cs
@@ -21,5 +21,28 @@ namespace AAY
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form4 form4 = new Form4();
+            form4.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form5 form5 = new Form5();
+            form5.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
